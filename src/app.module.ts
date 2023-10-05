f27cc07bb48cd2f140134f05c23ce9b6d3bbbbf3
Cristian Valencia <cvalencia@ifactum.com>
2023-10-05 15:48:57 -0500

    database - deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,23 @@ import { User } from './user.entity';
 import {JwtModule} from "@nestjs/jwt";
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '',
+    //   database: 'libreria',
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true, // Solo para desarrollo, desactivar en producción
+    // }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'sql9.freesqldatabase.com',
       port: 3306,
-      username: 'root',
-      password: '',
-      database: 'libreria',
+      username: 'sql9651341',
+      password: 'hflS6IwPfQ',
+      database: 'sql9651341',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // Solo para desarrollo, desactivar en producción
     }),
